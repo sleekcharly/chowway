96f7df5e701017171e10cdd827eb5a6ebc06d59a
sleekcharly <c_ukasoanya@yahoo.com>
2024-11-26 02:54:04 +0100

    Built the basket popup

---
+++ features/basketSlice.ts
@@ -0,0 +1,48 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { store } from '../store';
+
+const initialState = {
+  items: [],
+};
+
+export const basketSlice = createSlice({
+  name: 'basket',
+  initialState,
+  reducers: {
+    addToBasket: (state, action) => {
+      state.items = [...state.items, action.payload];
+    },
+    removeFromBasket: (state, action) => {
+      const index = state.items.findIndex(
+        (item) => item.id === action.payload.id,
+      );
+
+      let newBasket = [...state.items];
+
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Can't remove product (id: ${action.payload.id}) as its not set in basket`,
+        );
+      }
+
+      state.items = newBasket;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { addToBasket, removeFromBasket } = basketSlice.actions;
+
+// redux selector
+export const selectBasketItems = (state) => state.basket.items;
+
+// helper function to filter out items based on id
+export const selectBasketItemsWithId = (state, id) =>
+  state.basket.items.filter((item) => item.id === id);
+
+export const selectBasketTotal = (state) =>
+  state.basket.items.reduce((total, item) => (total += item.price), 0);
+
+export default basketSlice.reducer;
